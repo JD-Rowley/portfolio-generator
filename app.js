b177ee7be91b846bb0d759f9b0c33f496ea4651e
JD Rowley <jdrowley1989@gmail.com>
2021-09-10 14:44:01 -0600

    added function to generate HTML webpage

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var commandLineArgs = process.argv[1];
-console.log(commandLineArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
