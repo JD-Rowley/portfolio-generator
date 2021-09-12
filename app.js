7e7e864b893ae087b0b4f696156edc6f52567cc7
JD Rowley <jdrowley1989@gmail.com>
2021-09-11 19:10:23 -0600

    added choice to make about section

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -39,81 +39,95 @@ const promptUser = () => {
         }
     },
     {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+    },
+    {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
   ]);
 };
 
 const promptProject = portfolioData => {
-    // If there's no projects array property, create one
-    if (!portfolioData.projects) {
-        portfolioData.projects = [];
-    }
     console.log(`
     =================
     Add a New Project
     =================
     `);
-        return inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is the name of your project?'
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Provide a description of the project (Required)',
-                validate: projectDescription => {
-                    if (projectDescription) {
-                        return true;
-                    } else {
-                        console.log('Please enter a project description!');
-                        return false;
-                    }
+    // If there's no projects array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescription => {
+                if (projectDescription) {
+                    return true;
+                } else {
+                    console.log('Please enter a project description!');
+                    return false;
                 }
-            },
-            {
-                type: 'checkbox',
-                name: 'languages',
-                message: 'What did you build this project with? (Check all that apply)',
-                choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-            },
-            {
-                type: 'input',
-                name: 'link',
-                message: 'Enter the GitHub link to your project. (Required)',
-                validate: githubLink => {
-                    if (githubLink) {
-                        return true;
-                    } else {
-                        console.log('Please enter your GitHub project link!');
-                        return false;
-                    }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: githubLink => {
+                if (githubLink) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub project link!');
+                    return false;
                 }
-            },
-            {
-                type: 'confirm',
-                name: 'feature',
-                message: 'Would you like to feature this project?',
-                default: false
-            },
-            {
-                type: 'confirm',
-                name: 'confirmAddProject',
-                message: 'Would you like to enter another project?',
-                default: false
             }
-        ]).then(projectData => {
-            portfolioData.projects.push(projectData);
-            if (projectData.confirmAddProject) {
-                return promptProject(portfolioData);
-            } else {
-                return portfolioData;
-            }
-        });
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
 };
 
 promptUser()
