f2aa827f8501682b75e7d1cd52c44c72ff9514e8
JD Rowley <jdrowley1989@gmail.com>
2021-09-13 15:27:59 -0600

    code refactor

---
+++ utils/generate-site.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            // if there's an error, reject promise
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            // if all went well, resolve the promise
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Style sheet copied successfully!'
+            });
+        });
+    });
+};
+
+module.exports = { writeFile, copyFile }; 
